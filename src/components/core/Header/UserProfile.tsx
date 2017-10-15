8e7325cead8761f23e545f1b593b59816139878a
Michel Costa <mikelfcosta@live.com>
2017-10-15 15:46:15 -0200

    Updated userprofile default image and typo

--- src/components/core/Header/UserProfile.tsx
+++ src/components/core/Header/UserProfile.tsx
@@ -10,7 +10,7 @@ export default class UserProfile extends React.Component<{}, {}> {
   render() {
     return (
       <div className={userProfile}>
-        <img srcSet={'img/default-avatar.png'} />
+        <img srcSet={'img/default-avatar.jpg'} />
         <div>
           <h3>Michel Costa</h3>
           <h4>Admin</h4>
