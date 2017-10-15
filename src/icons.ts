74aab13ad8ef49ee8d9037eb4ceec33d9c4d9b9c
Michel Costa <mikelfcosta@live.com>
2017-10-15 15:10:06 -0200

    Added nav icons

--- src/icons.ts
+++ src/icons.ts
@@ -1,3 +1,6 @@
 const iconPath = '/img/icons/ico-';
 
 export const NOTIFICATION = iconPath + 'notification.svg';
+export const HOME = iconPath + 'home.svg';
+export const USERS = iconPath + 'user.svg';
+export const INSIGHTS = iconPath + 'insights.svg';
