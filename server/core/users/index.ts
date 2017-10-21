9c7872d68a5ae26dcb40181753456ee8a7b38144
Michel Costa <mikelfcosta@live.com>
2017-10-21 20:10:59 -0200

    Set up users routes

--- server/core/users/index.ts
+++ server/core/users/index.ts
@@ -1,5 +1,22 @@
 import { Router } from 'express';
+import login from './api/login';
+import logout from './api/logout';
+import getUsers from './api/users/getUsers';
+import getUserById from './api/users/getUserById';
+import createUser from './api/users/createUser';
+import updateUser from './api/users/updateUser';
+import deleteUser from './api/users/deleteUser';
 
 const router = Router();
+
+router.get('/login', login);
+router.get('/logout', logout);
+
+router.get('/users', getUsers);
+router.post('/users/new', createUser);
+router.route('/users/:_id')
+  .get(getUserById)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 export default router;
