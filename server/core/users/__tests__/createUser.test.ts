68ea9d23dd72265f6e0087013985570961b3e594
Michel Costa <mikelfcosta@live.com>
2017-10-21 20:57:36 -0200

    Set up first test with jest

--- server/core/users/__tests__/createUser.test.ts
+++ server/core/users/__tests__/createUser.test.ts
@@ -1,13 +1,30 @@
 import * as supertest from 'supertest';
 import App from '../../../App';
 import { OmniRouter } from '../../../Router';
+import { omniUsers } from '../models/Users';
 
 const agent = supertest.agent(App);
 const core = OmniRouter.coreApi;
 const path = `${core}/users/new`;
 
 describe('[Core] Create Users Tests', () => {
-  test('creates a new user with correct info');
+  test('creates a new user with correct info', (done) => {
+    const userToCreate = { username: 'CREATED.USER', password: 'created1234' };
+    agent
+      .post(path)
+      .send(userToCreate)
+      .expect(200)
+      .end(async (err, res) => {
+        if (err) return done(err);
+        try {
+          const user = await omniUsers.findById(userToCreate.username);
+          expect(user).toContain({ _id: userToCreate.username });
+          return done();
+        } catch (err) {
+          return done(err);
+        }
+      });
+  });
   test('does not create a new user with incorrect info');
   test('does not create a new user on unauthorized access');
   test('does not create a new user on unauthenticated access');
