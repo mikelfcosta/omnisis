e706cb36c3ad38ee424bf6f50a94bb3e9c39618e
Michel Costa <mikelfcosta@live.com>
2017-10-22 18:06:21 -0200

    Added methods to Users, updated exported Roles

--- server/core/users/models/Users.ts
+++ server/core/users/models/Users.ts
@@ -1,4 +1,5 @@
 import { Schema, Document, model, NativeError, Model } from 'mongoose';
+import './Roles';
 import * as bcrypt from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
 
@@ -19,10 +20,17 @@ export interface IOmniUsers extends Document {
 }
 
 export interface IOmniUsersModel extends Model<IOmniUsers> {
-  generateAuthToken: (userinfo: any) => Promise<IOmniUsers>;
+  generateAuthToken: (userinfo: any) => Promise<string>;
   removeToken: (_id: string) => void;
   findByToken: (token: string) => IOmniUsersModel | null;
   findByCredentials: (username: string, password: string) => IOmniUsers | null;
+}
+
+export enum ELoginErrors {
+  NoUser = 'Usuário não encontrado',
+  InactiveUser = 'Usuário inativo',
+  NoPassword = 'Você ainda não definiu sua senha',
+  IncorrectPassword = 'Senha incorreta',
 }
 
 class Users {
@@ -30,7 +38,6 @@ class Users {
 
   constructor() {
     this.setSchema();
-    this.schema.loadClass(Users);
     this.schema.pre('save', this.hashPassword);
     this.schema.pre('update', this.hashPassword);
   }
@@ -39,12 +46,12 @@ class Users {
     this.schema = new Schema({
       _id: { type: String, required: true },
       password: { type: String, required: true, default: null },
-      roles: [{ type: Schema.Types.ObjectId, ref: 'OmniRoles' }],
+      roles: [{ type: Schema.Types.ObjectId, ref: 'OmniUsersRoles' }],
       active: { type: Boolean, default: false },
       createdAt: { type: Date, default: Date.now },
       createdBy: { type: String, ref: 'OmniUsers', required: true },
       lastLogin: { type: Date, default: null },
-      tokens: [{ access: String, token: String }],
+      tokens: [{ _id: false, access: String, token: String }],
       profile: {
         _id: false,
         name: String,
@@ -65,11 +72,14 @@ class Users {
     } else next();
   }
 
-  public async generateAuthToken(this: IOmniUsers, userinfo: IOmniUsers) {
-    const token = jwt.sign(userinfo, 'omniforyou').toString(); // Create a token with a secret
-    this.tokens.push({ token, access: 'auth' }); // Add the token to the user document
-    await this.save(); // Save the token and return it
-    return this.tokens;
+  static async generateAuthToken(this: IOmniUsers, user: IOmniUsers) {
+    const userToTokenize = <IOmniUsers>user.toObject();
+    delete userToTokenize.password;
+    delete userToTokenize.tokens;
+    const token = jwt.sign(userToTokenize, 'omniforyou').toString(); // Create a token with a secret
+    user.tokens.push({ token, access: 'auth' }); // Add the token to the user document
+    await user.save(); // Save the token and return it
+    return token;
   }
 
   public static async removeToken(this: IOmniUsersModel, _id: string) {
@@ -94,16 +104,14 @@ class Users {
 
   public static async findByCredentials(this: IOmniUsersModel, username: string, password: string) {
     try {
-      const user = <IOmniUsers>await this.findById(username, 'profile password roles active').populate('roles').lean();
-      if (!user) return Promise.reject(0);
-      else if (!user.active) return Promise.reject(1);
-      else if (user.password === null) return Promise.reject(2);
+      const user = <IOmniUsers>await this.findById(username, 'profile password roles active tokens').populate('roles');
+      if (!user) return Promise.reject(ELoginErrors.NoUser);
+      else if (!user.active) return Promise.reject(ELoginErrors.InactiveUser);
+      else if (user.password === null) return Promise.reject(ELoginErrors.NoPassword);
 
       const correctPassword = await bcrypt.compare(password, user.password);
-      delete user.password;
-
       if (correctPassword) return user;
-      else return Promise.reject(3);
+      else return Promise.reject(ELoginErrors.IncorrectPassword);
     } catch (err) {
       throw err;
     }
@@ -111,5 +119,8 @@ class Users {
 
 }
 
-export const omniUsers: IOmniUsersModel = model<IOmniUsers, IOmniUsersModel>('OmniUsers', new Users().schema);
+const schema = new Users().schema;
+schema.loadClass(Users);
+
+export const omniUsers: IOmniUsersModel = model<IOmniUsers, IOmniUsersModel>('OmniUsers', schema);
 export default omniUsers;
