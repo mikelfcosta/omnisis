b284584eefe647d978fcb6b5d2936d42b23f6537
Michel Costa <mikelfcosta@live.com>
2017-10-22 14:12:27 -0200

    Added login and logout methods

--- server/core/users/models/Users.ts
+++ server/core/users/models/Users.ts
@@ -1,5 +1,6 @@
 import { Schema, Document, model, NativeError } from 'mongoose';
 import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 
 export interface IOmniUsersModel extends Document {
   _id: string;
@@ -9,6 +10,7 @@ export interface IOmniUsersModel extends Document {
   createdAt: Date;
   createdBy: Schema.Types.ObjectId;
   lastLogin: Date;
+  tokens: { access: string, token: string }[];
   profile?: {
     name?: string;
     id?: string;
@@ -35,6 +37,7 @@ class Users {
       createdAt: { type: Date, default: Date.now },
       createdBy: { type: String, ref: 'OmniUsers', required: true },
       lastLogin: { type: Date, default: null },
+      tokens: [{ access: String, token: String }],
       profile: {
         _id: false,
         name: String,
@@ -44,7 +47,7 @@ class Users {
     });
   }
 
-  async hashPassword(this: IOmniUsersModel, next: (err?: NativeError) => void) {
+  private async hashPassword(this: IOmniUsersModel, next: (err?: NativeError) => void) {
     if (this.isModified('password')) {
       try {
         this.password = await bcrypt.hash(<string>this.password, 8);
@@ -55,7 +58,48 @@ class Users {
     } else next();
   }
 
-}
+  public async generateAuthToken(this: IOmniUsersModel, userinfo: any) {
+    const token = jwt.sign(userinfo, 'omniforyou').toString(); // Create a token with a secret
+    this.tokens.push({ token, access: 'auth' }); // Add the token to the user document
+    return await this.save(); // Save the token and return it
+  }
 
+  public static async removeToken(this: any, _id: string) {
+    return await this.findByIdAndUpdate(_id, { tokens: [] }, { new: true });
+  }
+
+  public static async findByToken(this: any, token: string) {
+    try {
+      const decoded = <IOmniUsersModel>jwt.verify(token, 'omniforyou');
+      return await this.findOne({
+        _id: decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth',
+      })
+        .populate('role', 'name level')
+        .lean();
+
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public static async findByCredentials(this: any, username: string, password: string) {
+    try {
+      const user = this.findById(username)
+        .populate('role permissions');
+      if (!user) return Promise.reject(0);
+      else if (!user.active) return Promise.reject(1);
+      else if (user.password === null) return Promise.reject(2);
+
+      const correctPassword = await bcrypt.compare(password, user.password);
+      if (correctPassword) return user;
+      else return Promise.reject(3);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+}
 
 export const omniUsers = model<IOmniUsersModel>('OmniUsers', new Users().schema);
