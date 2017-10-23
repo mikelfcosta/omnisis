926f4e9f5d3428a8912d371cc6ec79404004789c
Michel Costa <mikelfcosta@live.com>
2017-10-23 20:34:54 -0200

    Finished populateCards helper

---
+++ server/modules/iot/__tests__/helpers.ts
@@ -0,0 +1,29 @@
+import * as faker from 'faker';
+import { IOmniSmartCards, omniSmartCards } from '../models/Cards';
+
+export async function populateCards(smartCardsToCreate: number = 10) {
+  let smartCardsCreated = 3;
+  const smartCards: IOmniSmartCards[] = [];
+  const activeCard = new omniSmartCards({ _id: 'tagteste00001', assigned: true, student: '20709639', active: true, lastAssignedBy: 'michel.costa', lastAssignedAt: new Date().getTime() });
+  const inactiveCard = new omniSmartCards({ _id: faker.internet.ipv6(), assigned: true, student: faker.internet.userName(), active: false });
+  const unassignedCard = new omniSmartCards({ _id: faker.internet.ipv6(), assigned: false });
+  try {
+    smartCards.push(await activeCard.save());
+    smartCards.push(await inactiveCard.save());
+    smartCards.push(await unassignedCard.save());
+    for (smartCardsCreated; smartCardsCreated < smartCardsToCreate; smartCardsCreated += 1) {
+      const isAssigned = faker.random.boolean();
+      const smartCard = new omniSmartCards({
+        _id: faker.internet.ipv6(),
+        assigned: isAssigned,
+        student: isAssigned ? faker.internet.userName() : null,
+        active: isAssigned ? faker.random.boolean() : false,
+      });
+      smartCards.push(await smartCard.save());
+    }
+    return Promise.all(smartCards);
+  } catch (err) {
+    console.error(err);
+    return Promise.reject(err);
+  }
+}
