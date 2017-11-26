d0e6269069908c8159a9e0540b658881c0c7297a
Michel Costa <mikelfcosta@live.com>
2017-11-26 02:00:51 -0200

    Added Skeleton for Main Areas

---
+++ src/components/Insights/Insights.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export default class Insights extends React.Component<{}, {}> {
+
+  componentWillMount() {}
+
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
