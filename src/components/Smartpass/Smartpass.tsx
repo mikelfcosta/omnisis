3f4d53e6bddfc3f5d6e974b8504f31c051820ef7
Michel Costa <mikelfcosta@live.com>
2017-11-26 02:11:41 -0200

    Added Routers to Smartpass

--- src/components/Smartpass/Smartpass.tsx
+++ src/components/Smartpass/Smartpass.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import SmartMachines from './Machines/SmartMachines';
 import SmartCards from './Cards/SmartCards';
 import SmartLocations from './Locations/SmartLocations';
+import { Redirect } from 'react-router';
 
 export default class Smartpass extends React.Component<{}, {}> {
 
@@ -13,6 +14,7 @@ export default class Smartpass extends React.Component<{}, {}> {
   render() {
     return (
       <Switch>
+        <Redirect to="/iot/machines"/>
         <Route path="/iot/machines" component={SmartMachines}/>
         <Route path="/iot/cards" component={SmartCards}/>
         <Route path="/iot/locations" component={SmartLocations}/>
