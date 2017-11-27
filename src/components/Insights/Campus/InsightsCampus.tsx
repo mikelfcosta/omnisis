0b857bbb52d24d689da84cf092ed51e4f26f0481
Michel Costa <mikelfcosta@live.com>
2017-11-27 02:53:13 -0200

    Setup InsightsCampus ChartPlaceholder

--- src/components/Insights/Campus/InsightsCampus.tsx
+++ src/components/Insights/Campus/InsightsCampus.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import Card from '../../core/Content/Card';
+import { cardHeader } from '../../core/Content/Card.scss';
+import { chart } from './InsightsCampus.scss';
 
 export default class InsightsCampus extends React.Component<{}, {}> {
 
@@ -9,7 +12,38 @@ export default class InsightsCampus extends React.Component<{}, {}> {
   render() {
     return (
       <div>
-        <p>InsightsCampus component works!</p>
+        <Card size={100}>
+          <div className={cardHeader}>
+            Acessos por Campus
+          </div>
+          <div className={chart}>
+            <p>ChartType: Lines</p>
+            <p>ChartData: AccessByCampus</p>
+            <p>ChartOptions: StartDate-EndDate</p>
+            <p>ChartLegend: [Each of the Campus]</p>
+          </div>
+        </Card>
+        <Card size={60}>
+          <div className={cardHeader}>
+            Acessos por Máquinas
+          </div>
+          <div className={chart}>
+            <p>ChartType: Pizza</p>
+            <p>ChartData: AccessByMachina</p>
+            <p>ChartOptions: Filter by Campus, StartDate-EndDate</p>
+            <p>ChartLegend: [Creation, Activation, Assignments, Unassignments, Inactivations]</p>
+          </div>
+        </Card>
+        <Card size={40}>
+          <div className={cardHeader}>
+            Ativos por Campus
+          </div>
+          <div className={chart}>
+            <p>ChartType: Pizza</p>
+            <p>ChartData: ActivesByCampus</p>
+            <p>ChartLegend: [Each of the Campus]</p>
+          </div>
+        </Card>
       </div>
     );
   }
