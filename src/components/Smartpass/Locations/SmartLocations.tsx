8a72e7d0e895945e5ea98efa4a7ad5b60c7c2779
Michel Costa <mikelfcosta@live.com>
2017-11-27 02:08:17 -0200

    Added default table to SmartLocations

--- src/components/Smartpass/Locations/SmartLocations.tsx
+++ src/components/Smartpass/Locations/SmartLocations.tsx
@@ -1,16 +1,44 @@
 import * as React from 'react';
+import TableCard, { TableCardState } from '../../core/Content/TableCard';
 
-export default class SmartLocations extends React.Component<{}, {}> {
+interface SmartLocationsData {
+  _id: string;
+  location: string;
+  machines: string;
+  createdBy: string;
+  updatedAt: string;
+}
 
-  componentWillMount() {}
+interface SmartLocationsState {
+  data: SmartLocationsData[];
+}
+
+const data: SmartLocationsData[] = [
+  { _id: '000004', location: 'Morumbi - Entrada Frontal', machines: '4', createdBy: 'michel.costa', updatedAt: '16/11/2017' },
+  { _id: '000003', location: 'Vila Olímpia - Academia', machines: '2', createdBy: 'michel.costa', updatedAt: '16/11/2017' },
+  { _id: '000002', location: 'Vila Olímpia - Prédio A', machines: '3', createdBy: 'michel.costa', updatedAt: '15/11/2017' },
+  { _id: '000001', location: 'Paulista I - Entrada Principal', machines: '4', createdBy: 'michel.costa', updatedAt: '15/11/2017' },
+];
+
+export default class SmartLocations extends React.Component<{}, SmartLocationsState> {
+  private headers = ['ID', 'Local', 'Maquinas', 'Criado por', 'Ultima Atualização'];
+
+  componentWillMount() {
+    this.setState({ data });
+  }
 
   componentWillUnmount() {}
 
   render() {
     return (
       <div>
-        <p>SmartLocations component works!</p>
+        <TableCard data={this.state.data} headers={this.headers}
+                   rowKey={'_id'} length={12} onPaginate={this.getData} />
       </div>
     );
+  }
+
+  getData(event: TableCardState) {
+    console.log(event);
   }
 }
