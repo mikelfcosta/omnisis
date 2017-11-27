f43da47fe35f20b73e9ac36e6af25987809c8bfb
Michel Costa <mikelfcosta@live.com>
2017-11-27 02:35:31 -0200

    Setup InsightsCards ChartPlaceholder

--- src/components/Insights/Cards/InsightsCards.tsx
+++ src/components/Insights/Cards/InsightsCards.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import Card from '../../core/Content/Card';
+import { chart } from './InsightsCards.scss';
+import { cardHeader } from '../../core/Content/Card.scss';
 
 export default class InsightsCards extends React.Component<{}, {}> {
 
@@ -9,7 +12,26 @@ export default class InsightsCards extends React.Component<{}, {}> {
   render() {
     return (
       <div>
-        <p>InsightsCards component works!</p>
+        <Card size={100}>
+          <div className={cardHeader}>
+            Cartões
+          </div>
+          <div className={chart}>
+            <p>ChartType: Pizza</p>
+            <p>ChartData: CardsByStatus</p>
+            <p>ChartLegend: [Active, Inactive, Unassigned]</p>
+          </div>
+        </Card>
+        <Card size={100}>
+          <div className={cardHeader}>
+            Ativações e Inativações
+          </div>
+          <div className={chart}>
+            <p>ChartType: Lines</p>
+            <p>ChartData: AssignmentsByTime</p>
+            <p>ChartLegend: [Creation, Activation, Assignments, Unassignments, Inactivations]</p>
+          </div>
+        </Card>
       </div>
     );
   }
