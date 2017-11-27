0f8b594ab95811c269fe53c457c4d344037363d7
Michel Costa <mikelfcosta@live.com>
2017-11-27 02:11:25 -0200

    Added default table to AdminUsers

--- src/components/Admin/Users/AdminUsers.tsx
+++ src/components/Admin/Users/AdminUsers.tsx
@@ -1,16 +1,42 @@
 import * as React from 'react';
+import TableCard, { TableCardState } from '../../core/Content/TableCard';
 
-export default class AdminUsers extends React.Component<{}, {}> {
+interface AdminUsersData {
+  _id: string;
+  name: string;
+  status: string;
+  role: string;
+  createdBy: string;
+  updatedAt: string;
+}
 
-  componentWillMount() {}
+interface AdminUsersState {
+  data: AdminUsersData[];
+}
+
+const data: AdminUsersData[] = [
+  { _id: 'michel.costa', name: 'Michel Costa', status: 'Ativo', role: 'SUPER ADMIN', createdBy: 'michel.costa', updatedAt: '01/11/2017' },
+];
+
+export default class AdminUsers extends React.Component<{}, AdminUsersState> {
+  private headers = ['ID', 'Nome', 'Status', 'Perfil', 'Criado por', 'Ultima Atualização'];
+
+  componentWillMount() {
+    this.setState({ data });
+  }
 
   componentWillUnmount() {}
 
   render() {
     return (
       <div>
-        <p>AdminUsers component works!</p>
+        <TableCard data={this.state.data} headers={this.headers}
+                   rowKey={'_id'} length={1} onPaginate={this.getData} />
       </div>
     );
+  }
+
+  getData(event: TableCardState) {
+    console.log(event);
   }
 }
