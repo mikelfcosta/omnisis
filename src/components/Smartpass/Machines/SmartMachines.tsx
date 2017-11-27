b84a5a24f40b340252ce282af83b1a4b8da1bd95
Michel Costa <mikelfcosta@live.com>
2017-11-27 02:04:37 -0200

    Added locations data

--- src/components/Smartpass/Machines/SmartMachines.tsx
+++ src/components/Smartpass/Machines/SmartMachines.tsx
@@ -4,6 +4,7 @@ import TableCard, { TableCardState } from '../../core/Content/TableCard';
 interface SmartMachinesData {
   _id: string;
   active: string;
+  location: string;
   createdBy: string;
   updatedAt: string;
 }
@@ -13,20 +14,20 @@ interface SmartMachinesState {
 }
 
 const data: SmartMachinesData[] = [
-  { _id: '000012', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
-  { _id: '000011', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
-  { _id: '000010', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
-  { _id: '000009', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
-  { _id: '000008', active: 'Inativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
-  { _id: '000007', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
-  { _id: '000006', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
-  { _id: '000005', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
-  { _id: '000004', active: 'Inativo', createdBy: 'michel.costa', updatedAt: '11/11/2017' },
-  { _id: '000003', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '11/11/2017' },
+  { _id: '000012', active: 'Ativo', location: 'Morumbi - Entrada Frontal', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000011', active: 'Ativo', location: 'Morumbi - Entrada Frontal', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000010', active: 'Ativo', location: 'Morumbi - Entrada Frontal', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000009', active: 'Ativo', location: 'Morumbi - Entrada Frontal', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000008', active: 'Inativo', location: 'Vila Olímpia - Academia', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000007', active: 'Ativo', location: 'Vila Olímpia - Prédio A', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
+  { _id: '000006', active: 'Ativo', location: 'Vila Olímpia - Prédio A', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
+  { _id: '000005', active: 'Ativo', location: 'Vila Olímpia - Prédio A', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
+  { _id: '000004', active: 'Inativo', location: 'Paulista I - Entrada Principal', createdBy: 'michel.costa', updatedAt: '11/11/2017' },
+  { _id: '000003', active: 'Ativo', location: 'Paulista I - Entrada Principal', createdBy: 'michel.costa', updatedAt: '11/11/2017' },
 ];
 
 export default class SmartMachines extends React.Component<{}, SmartMachinesState> {
-  private headers = ['ID', 'Status', 'Criado por', 'Ultima Atualização'];
+  private headers = ['ID', 'Status', 'Local', 'Criado por', 'Ultima Atualização'];
 
   componentWillMount() {
     this.setState({ data });
