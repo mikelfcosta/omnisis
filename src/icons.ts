3d4f7deb47b8e9ee86c04a3dff5a0bcc9b170f0c
Michel Costa <mikelfcosta@live.com>
2017-11-27 04:07:07 -0200

    Added ADD icon

--- src/icons.ts
+++ src/icons.ts
@@ -3,3 +3,4 @@ export const HOME = require('./img/icons/ico-home.svg');
 export const USERS = require('./img/icons/ico-user.svg');
 export const INSIGHTS = require('./img/icons/ico-insights.svg');
 export const LISTSEARCH = require('./img/icons/ico-listsearch.svg');
+export const ADD = require('./img/icons/ico-add.svg');
