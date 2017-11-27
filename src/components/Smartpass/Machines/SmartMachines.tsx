0732bf800d0b637f894c3419d2e740b0de486dfc
Michel Costa <mikelfcosta@live.com>
2017-11-27 01:51:50 -0200

    Finished SmartMachines

--- src/components/Smartpass/Machines/SmartMachines.tsx
+++ src/components/Smartpass/Machines/SmartMachines.tsx
@@ -1,16 +1,49 @@
 import * as React from 'react';
+import TableCard, { TableCardState } from '../../core/Content/TableCard';
 
-export default class SmartMachines extends React.Component<{}, {}> {
+interface SmartMachinesData {
+  _id: string;
+  active: string;
+  createdBy: string;
+  updatedAt: string;
+}
 
-  componentWillMount() {}
+interface SmartMachinesState {
+  data: SmartMachinesData[];
+}
+
+const data: SmartMachinesData[] = [
+  { _id: '000012', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000011', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000010', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000009', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000008', active: 'Inativo', createdBy: 'michel.costa', updatedAt: '13/11/2017' },
+  { _id: '000007', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
+  { _id: '000006', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
+  { _id: '000005', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '12/11/2017' },
+  { _id: '000004', active: 'Inativo', createdBy: 'michel.costa', updatedAt: '11/11/2017' },
+  { _id: '000003', active: 'Ativo', createdBy: 'michel.costa', updatedAt: '11/11/2017' },
+];
+
+export default class SmartMachines extends React.Component<{}, SmartMachinesState> {
+  private headers = ['ID', 'Status', 'Criado por', 'Ultima Atualização'];
+
+  componentWillMount() {
+    this.setState({ data });
+  }
 
   componentWillUnmount() {}
 
   render() {
     return (
       <div>
-        <p>SmartMachines component works!</p>
+        <TableCard data={this.state.data} headers={this.headers}
+                   rowKey={'_id'} length={12} onPaginate={this.getData} />
       </div>
     );
+  }
+
+  getData(event: TableCardState) {
+    console.log(event);
   }
 }
