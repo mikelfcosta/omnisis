9f0d93f403b78aad04654e653efdd9a249da026e
Michel Costa <mikelfcosta@live.com>
2017-11-30 20:58:52 -0200

    Set up populateCards to ensure user's creation with correct names and card assignement

--- server/modules/iot/__tests__/helpers.ts
+++ server/modules/iot/__tests__/helpers.ts
@@ -1,11 +1,22 @@
 import * as faker from 'faker';
 import { IOmniSmartCards, omniSmartCards } from '../models/Cards';
+import { omniHolders } from '../../holders/models/Holders';
+
+const whiteCard = '80787d7a';
+const keyChain = '80cd5280';
 
 export async function populateCards(smartCardsToCreate: number = 10) {
   let smartCardsCreated = 3;
   const smartCards: IOmniSmartCards[] = [];
-  const activeCard = new omniSmartCards({ _id: 'tagteste00001', assigned: true, student: '20709639', active: true, lastAssignedBy: 'michel.costa', lastAssignedAt: new Date().getTime() });
-  const inactiveCard = new omniSmartCards({ _id: faker.internet.ipv6(), assigned: true, lastAssignedBy: activeCard.lastAssignedBy, student: faker.internet.userName(), active: false });
+
+  await omniHolders.findByIdAndUpdate('20709639',
+    { _id: '20709639', name: 'Michel Augusto Favero Costa', type: 'user', activeCard: whiteCard },
+    { upsert: true });
+  await omniHolders.findByIdAndUpdate('20700000',
+    { _id: '20700000', name: 'João Vitor Barbosa Brandão', type: 'user', activeCard: whiteCard },
+    { upsert: true });
+  const activeCard = new omniSmartCards({ _id: whiteCard, assigned: true, student: '20709639', active: true, lastAssignedBy: 'michel.costa', lastAssignedAt: Date.now() });
+  const inactiveCard = new omniSmartCards({ _id: keyChain, assigned: true, student: '20700000', active: false, lastAssignedBy: activeCard.lastAssignedBy, lastAssignedAt: Date.now() });
   const unassignedCard = new omniSmartCards({ _id: faker.internet.ipv6(), lastAssignedBy: activeCard.lastAssignedBy, assigned: false });
   try {
     smartCards.push(await activeCard.save());
