ddecaf575745982514bf7338ce620ebd95d756c8
Michel Costa <mikelfcosta@live.com>
2017-11-30 19:47:54 -0200

    Finished up the checkCardById function

--- server/modules/iot/index.ts
+++ server/modules/iot/index.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import checkCardById from './api/checkCardById';
+import searchCardById from "./api/searchCardById";
+import createTag from "./api/createTag";
+
+export const PRIVATE_KEY = '7ec8336709bf8a8d71d0ffff69127903';
 
 const router = Router();
 
 router.get('/cards/:_id', checkCardById);
+router.get('/cards/search/:_id', searchCardById);
+router.get('/cards/new/:_id', createTag);
 
 export default router;
