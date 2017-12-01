6c9da79f4e6c6c5663643e65fba6475b5308297a
Michel Costa <mikelfcosta@live.com>
2017-12-01 21:06:44 -0200

    Setup modal to add groups

--- src/components/Holders/Groups/HoldersGroups.tsx
+++ src/components/Holders/Groups/HoldersGroups.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import TableCard, { TableCardState } from '../../core/Content/TableCard';
+import FabButton from '../../core/Elements/FabButton';
+import Modal from 'reactstrap/lib/Modal';
+import HoldersGroupsEdit from './HoldersGroupsEdit';
+import { ADD } from '../../../icons';
 
 interface HoldersGroupsData {
   _id?: string;
@@ -11,6 +15,7 @@ interface HoldersGroupsData {
 
 interface HoldersGroupsState {
   data: HoldersGroupsData[];
+  modal: boolean;
 }
 
 const data: HoldersGroupsData[] = [
@@ -35,11 +40,21 @@ export default class HoldersGroups extends React.Component<{}, HoldersGroupsStat
       <div>
         <TableCard data={this.state.data} headers={this.headers}
                    rowKey={'name'} length={5} onPaginate={this.getData} />
+        <FabButton icon={ADD} onClick={this.toggle.bind(this)} />
+        <Modal isOpen={this.state.modal} toggle={this.toggle.bind(this)}>
+          <HoldersGroupsEdit toggle={this.toggle.bind(this)} />
+        </Modal>
       </div>
     );
   }
 
   getData(event: TableCardState) {
     console.log(event);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
+    });
   }
 }
