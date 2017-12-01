c305ab430906aac78fc4c6773644ed5aa1513ead
Michel Costa <mikelfcosta@live.com>
2017-12-01 20:24:05 -0200

    Changed import to require to prevent errors

--- server/modules/iot/common/normalizeName.ts
+++ server/modules/iot/common/normalizeName.ts
@@ -1,4 +1,4 @@
-import * as slugify from 'slugify';
+const slugify = require('slugify');
 import normalizeLength from './normalizeLength';
 
 export default function normalizeName(name: string) {
@@ -6,7 +6,7 @@ export default function normalizeName(name: string) {
   const studentNameWithSpecialCharacters = `${studentNameArray[0]} ${studentNameArray[studentNameArray.length - 1]}`
     .substr(0, 16);
 
-  const studentNameSlugified = (<string>slugify(studentNameWithSpecialCharacters)).toUpperCase();
+  const studentNameSlugified = slugify(studentNameWithSpecialCharacters).toUpperCase();
   const studentNameNormalized = normalizeLength(studentNameSlugified);
 
   return studentNameNormalized.replace('-', ' ');
