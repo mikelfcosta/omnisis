7a1d9b030b652b70c505c79e6833165e00659aa2
Michel Costa <mikelfcosta@live.com>
2017-12-01 21:27:41 -0200

    Set up Locations Model

---
+++ server/modules/locations/models/OmniLocations.ts
@@ -0,0 +1,32 @@
+import { Schema, Document, Model, model } from 'mongoose';
+
+export interface IOmniLocations extends Document {
+  _id: Schema.Types.ObjectId;
+}
+
+export interface IOmniLocationsModel extends Model<IOmniLocations> {}
+
+class OmniLocations {
+  public schema: Schema;
+
+  constructor() {
+    this.setSchema();
+  }
+
+  setSchema() {
+    this.schema = new Schema({
+      campus: { type: String, required: true },
+      name: { type: String, required: true },
+      createdAt: { type: Date, default: Date.now },
+      createdBy: { type: String, required: true, ref: 'OmniUsers' },
+      lastUpdatedAt: { type: String, default: Date.now },
+      lastUpdatedBy: { type: Date, required: true, ref: 'OmniUsers' },
+    });
+  }
+}
+
+const schema = new OmniLocations().schema;
+schema.loadClass(OmniLocations);
+
+export const omniLocations: IOmniLocationsModel = model<IOmniLocations, IOmniLocationsModel>('OmniLocations', schema);
+export default omniLocations;
