1b253e7b82f4a802eac8feb294c99475ed82d383
Michel Costa <mikelfcosta@live.com>
2017-12-03 17:36:52 -0200

    Finished locationsSeed

---
+++ utilities/seed/locationsSeed.ts
@@ -0,0 +1,32 @@
+import { random } from 'lodash';
+import { models } from '../seed';
+
+export async function locationsSeed() {
+  await models.omniLocations.remove({});
+  const campi = await models.omniCampi.find({});
+  const users = ['michel.costa', 'joao.vitor'];
+
+  const locationsToSave = [];
+  try {
+    for (let i = 0; i < campi.length; i += 1) {
+      const user = users[random(0, 1)];
+      const mainLocation = new models.omniLocations({
+        campus: campi[i]._id,
+        name: 'Entrada Principal',
+        createdBy: user,
+        lastUpdatedBy: user,
+      });
+      const secondaryLocation = new models.omniLocations({
+        campus: campi[i]._id,
+        name: 'Entrada Secundária',
+        createdBy: user,
+        lastUpdatedBy: user,
+      });
+      locationsToSave.push(await mainLocation.save());
+      locationsToSave.push(await secondaryLocation.save());
+    }
+    return Promise.all(locationsToSave);
+  } catch (err) {
+    console.error(err);
+  }
+}
