879da73dadc4238f50be07f3e4023078b4df2e30
Michel Costa <mikelfcosta@live.com>
2017-12-03 17:26:45 -0200

    added campusSeed to execution, added omniHoldersgroups to models

--- utilities/seed.ts
+++ utilities/seed.ts
@@ -1,17 +1,21 @@
 import * as mongoose from 'mongoose';
 import omniCampi from '../server/modules/locations/models/omniCampi';
+import omniHoldersGroups from '../server/modules/holders/models/omniHoldersGroups';
 import { groupsSeed } from './seed/groupsSeed';
+import { campusSeed } from './seed/campusSeed';
 
 (<any>mongoose).Promise = Promise;
 mongoose.connect('mongodb://localhost:27017/iot', { useMongoClient: true });
 
 export const models = {
   omniCampi,
+  omniHoldersGroups,
 };
 
 export async function mainSeed() {
   try {
     await groupsSeed();
+    await campusSeed();
   } catch (err) {
     return Promise.reject(err);
   }
