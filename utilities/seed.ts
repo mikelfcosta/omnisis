c75ac2e1dde62cae6460d305beb1f5bf5b9e2e00
Michel Costa <mikelfcosta@live.com>
2017-12-03 17:35:36 -0200

    Imported omniLocations and executed locationsSeed

--- utilities/seed.ts
+++ utilities/seed.ts
@@ -1,8 +1,10 @@
 import * as mongoose from 'mongoose';
 import omniCampi from '../server/modules/locations/models/omniCampi';
 import omniHoldersGroups from '../server/modules/holders/models/omniHoldersGroups';
+import omniLocations from '../server/modules/locations/models/omniLocations';
 import { groupsSeed } from './seed/groupsSeed';
 import { campusSeed } from './seed/campusSeed';
+import { locationsSeed } from './seed/locationsSeed';
 
 (<any>mongoose).Promise = Promise;
 mongoose.connect('mongodb://localhost:27017/iot', { useMongoClient: true });
@@ -10,12 +12,14 @@ mongoose.connect('mongodb://localhost:27017/iot', { useMongoClient: true });
 export const models = {
   omniCampi,
   omniHoldersGroups,
+  omniLocations,
 };
 
 export async function mainSeed() {
   try {
     await groupsSeed();
     await campusSeed();
+    await locationsSeed();
   } catch (err) {
     return Promise.reject(err);
   }
