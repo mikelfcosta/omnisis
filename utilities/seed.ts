c71a3548df9372a967ce35279114bf32555dee89
Michel Costa <mikelfcosta@live.com>
2017-12-03 18:05:47 -0200

    Imported omniSmartCards and executed cardsSeed

--- utilities/seed.ts
+++ utilities/seed.ts
@@ -3,10 +3,12 @@ import omniCampi from '../server/modules/locations/models/omniCampi';
 import omniHoldersGroups from '../server/modules/holders/models/omniHoldersGroups';
 import omniLocations from '../server/modules/locations/models/omniLocations';
 import omniMachines from '../server/modules/locations/models/omniMachines';
+import omniSmartCards from '../server/modules/iot/models/omniSmartCards';
 import { groupsSeed } from './seed/groupsSeed';
 import { campusSeed } from './seed/campusSeed';
 import { locationsSeed } from './seed/locationsSeed';
 import machinesSeed from './seed/machinesSeed';
+import cardsSeed from './seed/cardsSeed';
 
 (<any>mongoose).Promise = Promise;
 mongoose.connect('mongodb://localhost:27017/iot', { useMongoClient: true });
@@ -16,6 +18,7 @@ export const models = {
   omniHoldersGroups,
   omniLocations,
   omniMachines,
+  omniSmartCards,
 };
 
 export async function mainSeed() {
@@ -24,6 +27,7 @@ export async function mainSeed() {
     await campusSeed();
     await locationsSeed();
     await machinesSeed();
+    await cardsSeed();
   } catch (err) {
     return Promise.reject(err);
   }
