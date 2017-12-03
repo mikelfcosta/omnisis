b6c0ee3d3368bb91016dece041b142c9c70c1298
Michel Costa <mikelfcosta@live.com>
2017-12-03 19:03:16 -0200

    Added model and interface

--- server/modules/holders/models/omniHoldersProfiles.ts
+++ server/modules/holders/models/omniHoldersProfiles.ts
@@ -1,7 +1,13 @@
-import { Schema, Document, Model, model } from 'mongoose';
+import { Schema, Document, Model, model, Types } from 'mongoose';
 
 export interface IOmniHoldersProfiles extends Document {
-  _id: Schema.Types.ObjectId;
+  _id: Types.ObjectId;
+  name: string;
+  locations: Types.ObjectId[];
+  createdAt: Date;
+  createdBy: string;
+  lastUpdatedAt: Date;
+  lastUpdatedBy: string;
 }
 
 export interface IOmniHoldersProfilesModel extends Model<IOmniHoldersProfiles> {}
@@ -15,7 +21,12 @@ class HoldersProfiles {
 
   setSchema() {
     this.schema = new Schema({
-
+      name: { type: String, required: true },
+      locations: [{ type: Schema.Types.ObjectId, ref: 'OmniLocations' }],
+      createdAt: { type: Date, default: Date.now },
+      createdBy: { type: String, required: true, ref: 'OmniUsers' },
+      lastUpdatedAt: { type: Date, default: Date.now },
+      lastUpdatedBy: { type: String, required: true, ref: 'OmniUsers' },
     });
   }
 }
