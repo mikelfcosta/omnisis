eef2f362120eca8a1fc0c2e11ed4c8707802aa9f
Michel Costa <mikelfcosta@live.com>
2017-12-03 17:26:56 -0200

    Finished campusSeed

---
+++ utilities/seed/campusSeed.ts
@@ -0,0 +1,23 @@
+import { random } from 'lodash';
+import { models } from '../seed';
+
+export async function campusSeed() {
+  await models.omniCampi.remove({});
+  const campi = ['Morumbi', 'Vila Olímpia', 'Paulista I', 'Paulista II', 'Mooca'];
+  const users = ['michel.costa', 'joao.vitor'];
+  const campusToSave = [];
+  try {
+    for (let i = 0; i < campi.length; i += 1) {
+      const user = users[random(0, 1)];
+      const newGroup = new models.omniCampi({
+        name: campi[i],
+        createdBy: user,
+        lastUpdatedBy: user,
+      });
+      campusToSave.push(await newGroup.save());
+    }
+    return Promise.all(campusToSave);
+  } catch (err) {
+    console.error(err);
+  }
+}
