17cd0ed3c0db4ff2d2c335354f6f85c9bba437b2
Michel Costa <mikelfcosta@live.com>
2017-12-03 19:45:55 -0200

    Made the assignnement time be random

--- utilities/seed/cardAssignSeed.ts
+++ utilities/seed/cardAssignSeed.ts
@@ -1,5 +1,6 @@
 import { models } from '../seed';
 import { random } from 'lodash';
+import * as moment from 'moment';
 
 export default async function cardAssignSeed() {
   try {
@@ -11,14 +12,18 @@ export default async function cardAssignSeed() {
       if (shouldAssign) {
         const holderToAssign = await models.omniHolders.findOne({ activeCard: null });
         if (holderToAssign) {
+          const dateString = `${random(7, 11)}.${random(1, 30)}.2017 ${random(10, 18)}.${random(10, 59)}`;
+          const date = moment(dateString, 'MM.DD.YYYY HH:mm').toDate();
           assignedCards.push(
-            await models.omniHolders.findByIdAndUpdate(holderToAssign._id, { activeCard: cardsToAssign[i]._id }),
+            await models.omniHolders.findByIdAndUpdate(holderToAssign._id, {
+              activeCard: cardsToAssign[i]._id,
+            }),
             await models.omniSmartCards.findByIdAndUpdate(cardsToAssign[i]._id, {
               assigned: true,
               student: holderToAssign._id,
               active: random(0, 100) < 95,
               lastAssignedBy: ['michel.costa', 'joao.vitor'][random(0, 1)],
-              lastAssignedAt: Date.now(),
+              lastAssignedAt: date,
             }),
           );
         }
