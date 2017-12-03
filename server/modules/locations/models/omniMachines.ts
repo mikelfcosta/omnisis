12a66fa8a21a212c3591117a6bdb282c266da436
Michel Costa <mikelfcosta@live.com>
2017-12-03 18:06:37 -0200

    Created omniMachines model

---
+++ server/modules/locations/models/omniMachines.ts
@@ -0,0 +1,39 @@
+import { Schema, Document, Model, model, Types } from 'mongoose';
+
+export interface IOmniMachines extends Document {
+  _id: Types.ObjectId;
+  campus: Types.ObjectId;
+  name: string;
+  machines: Types.ObjectId[];
+  createdAt: Date;
+  createdBy: string;
+  lastUpdatedAt: Date;
+  lastUpdatedBy: string;
+}
+
+export interface IOmniMachinesModel extends Model<IOmniMachines> {}
+
+class OmniMachines {
+  public schema: Schema;
+
+  constructor() {
+    this.setSchema();
+  }
+
+  setSchema() {
+    this.schema = new Schema({
+      status: { type: Boolean, default: true },
+      location: { type: Schema.Types.ObjectId, ref: 'OmniLocations' },
+      createdAt: { type: Date, default: Date.now },
+      createdBy: { type: String, required: true, ref: 'OmniUsers' },
+      lastUpdatedAt: { type: Date, default: Date.now },
+      lastUpdatedBy: { type: String, required: true, ref: 'OmniUsers' },
+    });
+  }
+}
+
+const schema = new OmniMachines().schema;
+schema.loadClass(OmniMachines);
+
+export const omniMachines: IOmniMachinesModel = model<IOmniMachines, IOmniMachinesModel>('OmniMachines', schema);
+export default omniMachines;
