f8845c96fbeccf5b968b3e83a05986431ea3cb3c
Michel Costa <mikelfcosta@live.com>
2017-12-03 18:25:33 -0200

    Created new special locations on seed

--- utilities/seed/locationsSeed.ts
+++ utilities/seed/locationsSeed.ts
@@ -24,6 +24,32 @@ export async function locationsSeed() {
       });
       locationsToSave.push(await mainLocation.save());
       locationsToSave.push(await secondaryLocation.save());
+
+      if (campi[i].name === 'Morumbi') {
+        const studioLocation = new models.omniLocations({
+          campus: campi[i]._id,
+          name: 'Estúdio',
+          createdBy: user,
+          lastUpdatedBy: user,
+        });
+        const bspLocation = new models.omniLocations({
+          campus: campi[i]._id,
+          name: 'BSP',
+          createdBy: user,
+          lastUpdatedBy: user,
+        });
+        locationsToSave.push(await studioLocation.save());
+        locationsToSave.push(await bspLocation.save());
+      } else if (campi[i].name === 'Vila Olímpia') {
+        const gymLocation = new models.omniLocations({
+          campus: campi[i]._id,
+          name: 'Academia',
+          createdBy: user,
+          lastUpdatedBy: user,
+        });
+        locationsToSave.push(await gymLocation.save());
+      }
+
       await models.omniCampi.findByIdAndUpdate(campi[i]._id, {
         $push: {
           locations: { $each: [mainLocation._id, secondaryLocation._id] },
