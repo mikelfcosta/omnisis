d0af6330f0d1ea4aac94cbf2fb55a8cae0ebffdf
Michel Costa <mikelfcosta@live.com>
2017-12-04 08:44:15 -0200

    Created Behavior APIs routes

--- server/modules/insights/index.ts
+++ server/modules/insights/index.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import getAccessByDay from './api/getAccessByDay';
+import getActiveInactiveCount from './api/getActiveInactiveCount';
+import getAverageAccessTime from './api/getAverageAccessTime';
+import getAverageInactiveTime from './api/getAverageInactiveTime';
 
 const router = Router();
 
 router.get('/behavior/accessByDay', getAccessByDay);
+router.get('/behavior/activeInactiveCount', getActiveInactiveCount);
+router.get('/behavior/averageAccessTime', getAverageAccessTime);
+router.get('/behavior/averageInactiveTime', getAverageInactiveTime);
 
 export default router;
