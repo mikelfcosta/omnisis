dbb43cd0f4e3e96cb6a5a1c44d68d59a56245b2d
Michel Costa <mikelfcosta@live.com>
2017-12-04 02:43:44 -0200

    Created /locations API router

--- server/modules/index.ts
+++ server/modules/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import iot from './iot';
 import holders from './holders';
+import locations from './locations';
 
 const router = Router();
 
 router.use('/iot', iot);
 router.use('/holders', holders);
+router.use('/locations', locations);
 
 export default router;
