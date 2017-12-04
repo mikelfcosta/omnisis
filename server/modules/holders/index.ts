e8be83d0b05246c7880723d16c930d44a13f8a8c
Michel Costa <mikelfcosta@live.com>
2017-12-04 02:25:06 -0200

    Created getProfiles route

--- server/modules/holders/index.ts
+++ server/modules/holders/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import getUsers from './api/getUsers';
 import getGroups from './api/getGroups';
+import getProfiles from './api/getProfiles';
 
 const router = Router();
 
 router.get('/manage', getUsers);
 router.get('/groups', getGroups);
+router.get('/profiles', getProfiles);
 
 export default router;
