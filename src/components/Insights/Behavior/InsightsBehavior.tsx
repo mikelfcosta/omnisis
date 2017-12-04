6bcf6b9f59642ab5daeb0ebcc71252cca2090c88
Michel Costa <mikelfcosta@live.com>
2017-12-04 08:00:38 -0200

    Added Access By Day chart

--- src/components/Insights/Behavior/InsightsBehavior.tsx
+++ src/components/Insights/Behavior/InsightsBehavior.tsx
@@ -2,12 +2,34 @@ import * as React from 'react';
 import Card from '../../core/Content/Card';
 import { cardHeader } from '../../core/Content/Card.scss';
 import { chart } from './InsightsBehavior.scss';
+import { MODULES } from '../../../constants';
+import axios from 'axios';
+import { LineChart, ResponsiveContainer, XAxis, CartesianGrid, Tooltip, Legend, Line } from 'recharts';
 
-export default class InsightsBehavior extends React.Component<{}, {}> {
+interface InsightsBehaviorState {
+  accessByDay: any[];
+}
 
-  componentWillMount() {}
+export default class InsightsBehavior extends React.Component<{}, InsightsBehaviorState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      accessByDay: [],
+    };
+  }
+
+  componentWillMount() {
+    this.getAccessByDay();
+  }
 
   componentWillUnmount() {}
+
+  getAccessByDay() {
+    axios.get(`${MODULES}/insights/behavior/accessByDay`)
+      .then(response => this.setState({ accessByDay: response.data }))
+      .catch(err => console.error(err));
+  }
 
   render() {
     return (
@@ -16,12 +38,16 @@ export default class InsightsBehavior extends React.Component<{}, {}> {
           <div className={cardHeader}>
             Acessos por Dia
           </div>
-          <div className={chart}>
-            <p>ChartType: Lines</p>
-            <p>ChartData: AccessesByDay</p>
-            <p>ChartOptions: Select StartDate/EndDate</p>
-            <p>ChartLegend: [ActiveAcesses, InactiveAcesses, Total]</p>
-          </div>
+          <ResponsiveContainer height={400}>
+            <LineChart height={400} data={this.state.accessByDay}
+                       margin={{ top: 40, right: 20, left: 20, bottom: 10 }}>
+              <XAxis dataKey="name"/>
+              <CartesianGrid strokeDasharray="2 3" vertical={false}/>
+              <Tooltip/>
+              <Legend />
+              <Line type="monotone" dataKey="access" stroke="#004751" />
+            </LineChart>
+          </ResponsiveContainer>
         </Card>
         <Card size={60}>
           <div className={cardHeader}>
