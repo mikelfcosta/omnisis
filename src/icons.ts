1da649356be21cde4224506c37bd91f638d25af3
Michel Costa <mikelfcosta@live.com>
2017-12-04 12:49:18 -0200

    Added assign and assign disabled icons

--- src/icons.ts
+++ src/icons.ts
@@ -7,3 +7,5 @@ export const ADD = require('./img/icons/ico-add.svg');
 export const CARDS = require('./img/icons/ico-cards.svg');
 export const EDIT = require('./img/icons/ico-edit.svg');
 export const ADMIN = require('./img/icons/ico-admin.svg');
+export const ASSIGN = require('./img/icons/ico-assign.svg');
+export const ASSIGN_DISABLED = require('./img/icons/ico-assign-disabled.svg');
