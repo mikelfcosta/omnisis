51e729b3202fd88ee7c2e337e6c77f98e00c2fbf
Michel Costa <mikelfcosta@live.com>
2017-12-04 08:41:11 -0200

    Set up getAccessByDay to get data from JSON file or generate JSON file

--- server/modules/insights/api/getAccessByDay.ts
+++ server/modules/insights/api/getAccessByDay.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as moment from 'moment';
 import omniSmartCardsLogs from '../../iot/models/omniSmartCardsLogs';
+import * as fs from 'fs'
 
 /**
  * Given a Card ID, check if the card exists, is assigned and validated.
@@ -11,14 +12,24 @@ import omniSmartCardsLogs from '../../iot/models/omniSmartCardsLogs';
  */
 export default async (req: Request, res: Response) => {
   try {
-    const data = [];
-    for (let i = 0; i <= 60; i += 1) {
-      const day = moment().subtract(i, 'd');
-      const query = { timestamp: { $gte: day.startOf('d').toDate(), $lte: day.endOf('d').toDate() } };
-      const access = await omniSmartCardsLogs.count(query);
-      data.push({ access, name: day.format('DD/MM') });
-    }
-    res.json(data.reverse());
+    const day = moment().subtract(1, 'd').format('DD.MM.YYYY');
+    return fs.readFile(`${__dirname}/data/getAccessByDay.${day}.json`, 'utf8', async (err, data) => {
+      if (err) console.error(err);
+      if (data) return res.json(JSON.parse(data));
+      const newData: any[] = [];
+      for (let i = 0; i <= 60; i += 1) {
+        const day = moment().subtract(i, 'd');
+        const query = { timestamp: { $gte: day.startOf('d').toDate(), $lte: day.endOf('d').toDate() } };
+        const access = await omniSmartCardsLogs.count(query);
+        newData.push({ access, name: day.format('DD/MM') });
+      }
+      newData.reverse();
+      return fs.writeFile(`${__dirname}/data/getAccessByDay.${day}.json`, JSON.stringify(newData), 'utf8', (err) => {
+        if (err) console.error(err);
+        res.json(newData);
+      });
+    });
+
 
   } catch (err) {
     console.error(err);
