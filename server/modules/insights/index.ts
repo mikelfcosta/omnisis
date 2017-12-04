b1c5dceea0c49d5b20f672dec308114877160866
Michel Costa <mikelfcosta@live.com>
2017-12-04 09:28:19 -0200

    Set up accessByCampus route

--- server/modules/insights/index.ts
+++ server/modules/insights/index.ts
@@ -3,6 +3,7 @@ import getAccessByDay from './api/getAccessByDay';
 import getActiveInactiveCount from './api/getActiveInactiveCount';
 import getAverageAccessTime from './api/getAverageAccessTime';
 import getAverageInactiveTime from './api/getAverageInactiveTime';
+import getAccessByCampus from './api/getAccessByCampus';
 
 const router = Router();
 
@@ -10,5 +11,9 @@ router.get('/behavior/accessByDay', getAccessByDay);
 router.get('/behavior/activeInactiveCount', getActiveInactiveCount);
 router.get('/behavior/averageAccessTime', getAverageAccessTime);
 router.get('/behavior/averageInactiveTime', getAverageInactiveTime);
+
+router.get('/campus/accessByCampus', getAccessByCampus);
+router.get('/campus/accessByMachine');
+router.get('/campus/activesByCampus');
 
 export default router;
