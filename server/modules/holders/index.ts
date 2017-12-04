3a77f92fba2ad9db408a1d872cb633d8e5d2c161
Michel Costa <mikelfcosta@live.com>
2017-12-04 02:13:57 -0200

    Set up getGroups route

--- server/modules/holders/index.ts
+++ server/modules/holders/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import getUsers from './api/getUsers';
+import getGroups from './api/getGroups';
 
 const router = Router();
 
-router.get('/manage/users', getUsers);
+router.get('/manage', getUsers);
+router.get('/groups', getGroups);
 
 export default router;
