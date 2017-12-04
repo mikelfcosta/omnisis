1f354b84481a0b05407c58d98e4cb3fd7bd21f43
Michel Costa <mikelfcosta@live.com>
2017-12-04 02:25:20 -0200

    Set up getting data from backend

--- src/components/Holders/Profile/HoldersProfiles.tsx
+++ src/components/Holders/Profile/HoldersProfiles.tsx
@@ -4,6 +4,8 @@ import HoldersProfileAdd from './HoldersProfileAdd';
 import FabButton from '../../core/Elements/FabButton';
 import { Modal } from 'reactstrap';
 import { ADD } from '../../../icons';
+import axios from 'axios';
+import { MODULES } from '../../../constants';
 
 interface HoldersProfilesData {
   _id?: string;
@@ -15,22 +17,29 @@ interface HoldersProfilesData {
 
 interface HoldersProfilesState {
   data: HoldersProfilesData[];
+  length: number;
   modal: boolean;
 }
-
-const data: HoldersProfilesData[] = [
-  { name: 'Acesso Básico', users: 15005, createdBy: 'michel.costa', updatedAt: '16/11/2017' },
-  { name: 'Studio', users: 80, createdBy: 'michel.costa', updatedAt: '14/11/2017' },
-  { name: 'Academia', users: 130, createdBy: 'michel.costa', updatedAt: '14/11/2017' },
-  { name: 'BSP', users: 650, createdBy: 'michel.costa', updatedAt: '08/11/2017' },
-  { name: 'Ateliês', users: 452, createdBy: 'michel.costa', updatedAt: '04/11/2017' },
-];
 
 export default class HoldersProfiles extends React.Component<{}, HoldersProfilesState> {
   private headers = ['Nome', 'Usuários', 'Criado por', 'Ultima Atualização'];
 
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      data: [],
+      length: 0,
+      modal: false,
+    };
+  }
+
   componentWillMount() {
-    this.setState({ data });
+    this.getData({
+      page: 0,
+      limit: 10,
+      order: 'name',
+      search: '',
+    });
   }
 
   componentWillUnmount() {}
@@ -39,7 +48,7 @@ export default class HoldersProfiles extends React.Component<{}, HoldersProfiles
     return (
       <div>
         <TableCard data={this.state.data} headers={this.headers}
-                   rowKey={'name'} length={5} onPaginate={this.getData} />
+                   rowKey={'name'} length={this.state.length} onPaginate={this.getData} />
         <FabButton icon={ADD} onClick={this.toggle.bind(this)} />
         <Modal isOpen={this.state.modal} toggle={this.toggle.bind(this)}>
           <HoldersProfileAdd toggle={this.toggle.bind(this)} />
@@ -49,7 +58,18 @@ export default class HoldersProfiles extends React.Component<{}, HoldersProfiles
   }
 
   getData(event: TableCardState) {
-    console.log(event);
+    axios.get(`${MODULES}/holders/profiles`, {
+      params: {
+        page: event.page,
+        limit: event.limit,
+        order: event.order,
+        search: event.search,
+      },
+    })
+      .then((response) => {
+        this.setState({ data: response.data.profiles, length: response.data.total });
+      })
+      .catch(err => console.error(err));
   }
 
   toggle() {
