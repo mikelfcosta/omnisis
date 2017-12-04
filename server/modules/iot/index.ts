e18d5fe29705382b8bcbfd582c0a14fd0727e031
Michel Costa <mikelfcosta@live.com>
2017-12-04 02:55:19 -0200

    Added getCards route

--- server/modules/iot/index.ts
+++ server/modules/iot/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import checkCardById from './api/checkCardById';
-import searchCardById from "./api/searchCardById";
-import createTag from "./api/createTag";
+import searchCardById from './api/searchCardById';
+import createTag from './api/createTag';
+import getCards from './api/getCards';
 
 export const PRIVATE_KEY = '7ec8336709bf8a8d71d0ffff69127903';
 
 const router = Router();
 
+router.get('/cards', getCards);
 router.get('/cards/search/:_id', searchCardById);
 router.get('/cards/new/:_id', createTag);
 router.get('/cards/:_id', checkCardById);
