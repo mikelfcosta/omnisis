d45855b95df7f21eb5846489980055f9b7c540a1
Michel Costa <mikelfcosta@live.com>
2017-12-04 12:49:37 -0200

    Made tableCard work with edit and assignemnt

--- src/components/core/Content/TableCard.tsx
+++ src/components/core/Content/TableCard.tsx
@@ -2,8 +2,7 @@ import * as React from 'react';
 import Card from './Card';
 import Table from 'reactstrap/lib/Table';
 import { tableCardToolbar, tableCardPagination } from './TableCard.scss';
-import { LISTSEARCH } from '../../../icons';
-import { debounce } from 'lodash';
+import { EDIT, LISTSEARCH, ASSIGN, ASSIGN_DISABLED } from '../../../icons';
 
 interface TableCardProps {
   rowKey: string;
@@ -12,6 +11,10 @@ interface TableCardProps {
   length: number;
   order?: string;
   onPaginate: (event: TableCardState) => any;
+  edit?: boolean;
+  assign?: boolean;
+  onEdit?: (e: React.MouseEvent<HTMLImageElement>) => any;
+  onAssign?: (e: React.MouseEvent<HTMLImageElement>) => any;
 }
 
 export interface TableCardState {
@@ -44,6 +47,8 @@ export default class TableCard extends React.Component<TableCardProps, TableCard
           <thead>
             <tr>
               {this.renderHeader()}
+              {this.props.edit ? <th>Editar</th> : null}
+              {this.props.assign ? <th>Assignar</th> : null}
             </tr>
           </thead>
           <tbody>
@@ -90,6 +95,8 @@ export default class TableCard extends React.Component<TableCardProps, TableCard
         return (
           <tr key={row[this.props.rowKey]}>
             {this.renderCells(row)}
+            {this.renderEdit(row)}
+            {this.renderAssign(row)}
           </tr>
         );
       }
@@ -117,6 +124,41 @@ export default class TableCard extends React.Component<TableCardProps, TableCard
     this.setState(change, () => {
       this.props.onPaginate(this.state);
     });
+  }
+
+  renderEdit(row: any) {
+    if (this.props.edit) {
+      return (
+        <th>
+          <img src={EDIT} alt="Editar" onClick={e => this.handleEdit(row)} style={ { width: '22px', marginLeft: '10px', cursor: 'pointer' } } />
+        </th>
+      );
+    }
+  }
+
+  handleEdit(row: any) {
+    if (this.props.onEdit) {
+      this.props.onEdit(row);
+    }
+  }
+
+  renderAssign(row: any) {
+    const icon = row.activeCard == null ? ASSIGN : ASSIGN_DISABLED;
+    const styles: any = { width: '22px', marginLeft: '20px' };
+    if (!row.activeCard) styles['cursor'] = 'pointer';
+    if (this.props.assign) {
+      return (
+        <th>
+          <img src={icon} alt="Assignar" onClick={e => this.handleAssign(row)} style={styles} />
+        </th>
+      );
+    }
+  }
+
+  handleAssign(row: any) {
+    if (this.props.onAssign) {
+      this.props.onAssign(row);
+    }
   }
 
 }
